5131b9d23e1fd5a286c7132f71c420a9e279c123
Thales <thalessilva.tv@gmail.com>
2022-02-25 12:07:23 -0300

    setup feito

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 require('dotenv').config();
+require('express-async-errors');
+
+const express = require('express');
+
+const { errorMiddleware } = require('./middlewares');
+
+const { productsRouter, salesRouter } = require('./routes');
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/products', productsRouter);
+app.use('/sales', salesRouter);
+
+app.use(errorMiddleware);
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
---
+++ routes/index.js
@@ -0,0 +1,7 @@
+const productsRouter = require('./products');
+const salesRouter = require('./sales');
+
+module.exports = {
+  productsRouter,
+  salesRouter,
+};
