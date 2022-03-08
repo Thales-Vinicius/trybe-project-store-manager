6c6878a25637c1560b7e983c7e9fb46402c46962
Thales <thalessilva.tv@gmail.com>
2022-03-08 09:56:40 -0300

    create middlwares product and sale

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,5 +1,9 @@
 const errorMiddleware = require('./errorMiddleware');
+const validateProduct = require('./validateProduct');
+const validateSale = require('./validateSale');
 
 module.exports = {
   errorMiddleware,
+  validateProduct,
+  validateSale,
 };
