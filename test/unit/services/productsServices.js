715d3b525567340ee39a691a095814956776bc81
Thales <thalessilva.tv@gmail.com>
2022-03-11 12:48:12 -0300

    add retornos errados products services

--- test/unit/services/productsServices.js
+++ test/unit/services/productsServices.js
@@ -3,7 +3,6 @@ const { expect } = require('chai');
 
 const productModels = require('../../../models/productsModels');
 const productServices = require('../../../services/productsServices');
-const { array } = require('joi');
 
 describe('Testando product services', () => {
   describe('Buscando por todos os produtos', () => {
@@ -91,6 +90,32 @@ describe('Testando product services', () => {
     });
   });
 
+  describe('Tentando cadastrar um produto existente', () => {
+    const mockTeste = [
+      {
+        "id": 1,
+        "name": "produto A",
+        "quantity": 10,
+      }
+    ];
+
+    before(() => {
+      sinon.stub(productModels, 'findByName').resolves(mockTeste);
+      sinon.stub(productModels, 'create').resolves(false);
+    });
+
+    after(() => {
+      productModels.findByName.restore();
+      productModels.create.restore();
+    });
+
+    it('retorna o produto cadastrado', async () => {
+      const serviceResponse = await productServices.create(mockTeste);
+
+      expect(serviceResponse).to.be.a('null');
+    });
+  });
+
   describe('dando update em um produto', () => {
     const mockTeste = [
       {
@@ -120,6 +145,32 @@ describe('Testando product services', () => {
     });
   });
 
+  describe('quando o produto a ser atualizado não existe', () => {
+    const mockTeste = [
+      {
+        "id": 1,
+        "name": "produto A",
+        "quantity": 10,
+      }
+    ];
+
+    before(() => {
+      sinon.stub(productModels, 'update').resolves(false);
+      sinon.stub(productModels, 'getById').resolves([]);
+    });
+
+    after(() => {
+      productModels.update.restore();
+      productModels.getById.restore();
+    });
+
+    it('retorna o produto atualizado', async () => {
+      const serviceResponse = await productServices.update(mockTeste);
+
+      expect(serviceResponse).to.be.a('null');
+    });
+  });
+
   describe('Deletando um produto', () => {
     const mockTeste = [
       {
@@ -143,6 +194,32 @@ describe('Testando product services', () => {
       const serviceResponse = await productServices.deleted(mockTeste);
 
       expect(serviceResponse).to.be.an('array');
+    });
+  });
+
+  describe('O produto a ser deletado não existe', () => {
+    const mockTeste = [
+      {
+        "id": 1,
+        "name": "produto A",
+        "quantity": 10,
+      }
+    ];
+
+    before(() => {
+      sinon.stub(productModels, 'getById').resolves([]);
+      sinon.stub(productModels, 'deleted').resolves(false);
+    });
+
+    after(() => {
+      productModels.getById.restore();
+      productModels.deleted.restore();
+    });
+
+    it('deleta o produto com sucesso', async () => {
+      const serviceResponse = await productServices.deleted(mockTeste);
+
+      expect(serviceResponse).to.be.a('null');
     });
   });
 });
